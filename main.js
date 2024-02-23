99547d13d914e2615f8dba11e092a038fc7a950b
g24force <102439759+g24force@users.noreply.github.com>
2024-02-23 00:58:23 +0000

    WIP Operate with 2 one digit numbers

--- main.js
+++ main.js
@@ -10,53 +10,56 @@ const divide = function(a, b) {
     return a / b;
 };
   
-const multiply = function(a) {
-    let total = 1;
-    for (let i = 0; i < a.length; i++) {
-      total = total * a[i];
-    }
-    return total;
+const multiply = function(a, b) {
+    return a * b;
 };
 
-const display = document.querySelector('#display');
+const display = document.querySelector('.display');
   
 let firstNumber;
 let operator;
 let secondNumber;
+let displayValue = 0;
 
 function operate (operator, a, b) {
-    switch (operator) {
-        case '+':
-            return add(a, b);
-        case '-':
-            return subtract(a, b);
-        case '/':
-                return divide(a, b);
-        case 'x':
-            return multiply(a, b);
+    a = Number(a);
+    b = Number(b);
+    if (operator === "/" && b === 0) {
+        return "You dummy!!! That's not possible xD";
+    } else {
+        switch (operator) {
+            case '+':
+                return add(a, b);
+            case '-':
+                return subtract(a, b);
+            case '/':
+                    return divide(a, b);
+            case 'x':
+                return multiply(a, b);
+        }
     }
 };
 
-let displayValue = 0;
+function populateDisplay (number) {
+    if (operator == undefined) {
+        displayValue = number;
+        firstNumber = displayValue;
+        display.textContent = displayValue;
+    } else {
+        displayValue = number;
+        secondNumber = displayValue;
+        display.textContent = displayValue;
+    }
+};
 
 const numberButtons = document.querySelectorAll('button.numbers');
-
 for (let i = 0; i < numberButtons.length; i++) {
     numberButtons[i].addEventListener('click', () => {
-        if (firstNumber !== undefined) {
-            displayValue += numberButtons[i].value;
-            secondNumber = numberButtons[i].value;
-            display.textContent = displayValue;
-        } else {
-            displayValue += numberButtons[i].value;
-            firstNumber = numberButtons[i].value;
-            display.textContent = displayValue;
-        }
+        populateDisplay (numberButtons[i].value);
     })
 };
 
 const operatorsButtons = document.querySelectorAll('button.operators');
-
 for (let i = 0; i < operatorsButtons.length; i++) {
     operatorsButtons[i].addEventListener('click', () => {
         operator = operatorsButtons[i].value;
@@ -64,19 +67,16 @@ for (let i = 0; i < operatorsButtons.length; i++) {
 };
 
 const clearButton = document.querySelector('.clear-button');
-
 clearButton.addEventListener('click', () => {
     display.textContent = 0;
     firstNumber = 0;
-    operator = null;
+    operator = undefined;
     secondNumber = 0;
     displayValue = 0;
 });
 
 const equalButton = document.querySelector('.equal-button');
-
 equalButton.addEventListener('click', () => {
-    console.log(operator, firstNumber, secondNumber)
     displayValue = operate(operator, firstNumber, secondNumber);
     display.textContent = displayValue;
 });
